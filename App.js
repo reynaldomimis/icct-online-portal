33d13663ea4e7f3ebc9ab8cb23aec7752a40be11
diether <diethergutierrez02@gmail.com>
2022-03-21 19:35:55 +0800

    <Text>Try x4</Text>

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ const App = () => {
   return (
     <View style={styles.root}>
       <Navigation />
+      <Text>Try 2</Text>
+      <Text>Try 2</Text>
+      <Text>Try 2</Text>
+      <Text>Try 2</Text>
     </View>
   );
 };
